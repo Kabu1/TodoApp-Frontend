1a750450203d00db478227f2501a69570ed760e2
kabu1 <bensonmk99@gmail.com>
2023-08-08 22:05:56 -0400

    change the api url to match that of the hosted backend service

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,3 @@
-// // import './assets/main.css'
-// import { createApp } from 'vue'
-// import App from './App.vue'
-// import router from './router'
-// import './assets/css/tailwind.css'
-// // import store from './store/modules/tasks'
-
-// const app = createApp(App)
-
-// app.use(router)
-// // app.use(store); // Use the Vuex store
-
-
-// app.mount('#app')
-
-
-// import { createApp } from 'vue';
-// import store from './path/to/store'; // Import your Vuex store
-
-// const app = createApp(App);
-// app.use(store); // Use the Vuex store
-// app.mount('#app');
-
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import  VuePageTransition  from 'vue-page-transition';
