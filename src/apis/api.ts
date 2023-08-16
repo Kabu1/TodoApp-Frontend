ceafb86fb76508d0e27dde61f3338809d4b7989a
kabu1 <bensonmk99@gmail.com>
2023-08-16 01:10:16 -0400

    cleanup

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -3,6 +3,7 @@ import axios from 'axios'
 import NProgress from 'nprogress';
 
 const base_url = import.meta.env.VITE_SOCKET_URL
+console.log('base url', base_url)
 
 export default () => {
     const api = axios.create({
